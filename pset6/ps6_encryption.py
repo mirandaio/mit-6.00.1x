441bd7f0153e2750000fee0a23243d0d7decacf7
Alfredo Miranda <alfredo@miranda.io>
2013-12-01 23:03:09 -0600

    Completed findBestShift function

--- pset6/ps6_encryption.py
+++ pset6/ps6_encryption.py
@@ -152,8 +152,23 @@ def findBestShift(wordList, text):
     text: string
     returns: 0 <= int < 26
     """
-    ### TODO
-    return "Not yet implemented." # Remove this comment when you code the function
+    text_words = text.split(' ')
+    max_valid = 0
+    best_shift = 0
+
+    for shift in range(26):
+        num_valid = 0
+
+        for word in text_words:
+            plaintext = applyShift(word, shift)
+            if isWord(wordList, plaintext):
+                num_valid += 1
+
+        if num_valid > max_valid:
+            max_valid = num_valid
+            best_shift = shift
+
+    return best_shift
 
 def decryptStory():
     """
