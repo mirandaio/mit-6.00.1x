e8fa232979fb5dc02d1b21acdcf64fdd5d27ce6e
Alfredo Miranda <alfredo@miranda.io>
2013-12-01 23:15:48 -0600

    Completed decryptStory function

--- pset6/ps6_encryption.py
+++ pset6/ps6_encryption.py
@@ -179,8 +179,10 @@ def decryptStory():
 
     returns: string - story in plain text
     """
-    ### TODO.
-    return "Not yet implemented." # Remove this comment when you code the function
+    ciphertext = getStoryString()
+    wordList = loadWords()
+    shift = findBestShift(wordList, ciphertext)
+    return applyShift(ciphertext, shift)
 
 #
 # Build data structures used for entire session and run encryption
