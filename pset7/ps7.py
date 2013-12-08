d057e04406e0b10e2772a1aacb6327e52898e000
Alfredo Miranda <alfredo@miranda.io>
2013-12-08 16:33:43 -0600

    Completed NewsStory and WordTrigger classes

--- pset7/ps7.py
+++ pset7/ps7.py
@@ -46,7 +46,29 @@ def process(url):
 
 # Problem 1
 
-# TODO: NewsStory
+class NewsStory(object):
+    def __init__(self, guid, the_title, the_subject, the_summary, link):
+        self.guid = guid
+        self.the_title = the_title
+        self.the_subject = the_subject
+        self.the_summary = the_summary
+        self.link = link
+
+    def getGuid(self):
+        return self.guid
+
+    def getTitle(self):
+        return self.the_title
+
+    def getSubject(self):
+        return self.the_subject
+
+    def getSummary(self):
+        return self.the_summary
+
+    def getLink(self):
+        return self.link
+        
 
 #======================
 # Part 2
@@ -65,6 +87,20 @@ class Trigger(object):
 # Problems 2-5
 
 # TODO: WordTrigger
+class WordTrigger(Trigger):
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def isWordIn(self, text):
+        text = replacePunc(text)
+        return self.word in text.lower().split()
+
+    def replacePunc(text):
+        for p in string.punctuation:
+            text = text.replace(p, ' ')
+
+        return text
+
 
 # TODO: TitleTrigger
 # TODO: SubjectTrigger
