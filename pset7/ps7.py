35a6061f6608b54ba57eb5d804e4a20007775255
Alfredo Miranda <alfredo@miranda.io>
2013-12-09 11:55:46 -0600

    Completed makeTrigger function

--- pset7/ps7.py
+++ pset7/ps7.py
@@ -186,14 +186,33 @@ def makeTrigger(triggerMap, triggerType, params, name):
 
     triggerMap: dictionary with names as keys (strings) and triggers as values
     triggerType: string indicating the type of trigger to make (ex: "TITLE")
-    params: list of strings with the inputs to the trigger constructor (ex: ["world"])
+    params: list of strings with the inputs to the trigger constructor 
+    (ex: ["world"])
     name: a string representing the name of the new trigger (ex: "t1")
 
     Modifies triggerMap, adding a new key-value pair for this trigger.
 
     Returns a new instance of a trigger (ex: TitleTrigger, AndTrigger).
     """
-    # TODO: Problem 11
+    trigger = None
+
+    if triggerType == "TITLE":
+        trigger = TitleTrigger(params[0])
+    elif triggerType == "SUBJECT":
+        trigger = SubjectTrigger(params[0])
+    elif triggerType == "SUMMARY":
+        trigger = SummaryTrigger(params[0])
+    elif triggerType == "NOT":
+        trigger = NotTrigger(triggerMap[params[0]])
+    elif triggerType == "AND":
+        trigger = AndTrigger(triggerMap[params[0]], triggerMap[params[1]])
+    elif triggerType == "OR":
+        trigger = OrTrigger(triggerMap[params[0]], triggerMap[params[1]])
+    elif triggerType == "PHRASE":
+        trigger = PhraseTrigger(" ".join(params))
+
+    triggerMap[name] = trigger
+    return trigger
 
 
 def readTriggerConfig(filename):
@@ -311,4 +330,3 @@ if __name__ == '__main__':
     root.title("Some RSS parser")
     thread.start_new_thread(main_thread, (root,))
     root.mainloop()
-
