4d7b5771c1a196608c387c7c11a291a728d4c990
Alfredo Miranda <alfredo@miranda.io>
2015-11-04 19:25:17 -0600

    Remove files

--- pset1/counting_bobs.py
+++
@@ -1,8 +0,0 @@
-s = "azcbobobegghakl"
-count = 0
-
-for i in range(0, len(s) - 2):
-    if s[i:i+3] == "bob":
-        count += 1
-
-print "Number of times bob occurs is: " + str(count)
--- pset1/counting_vowels.py
+++
@@ -1,8 +0,0 @@
-s = "azcbobobegghakl"
-count = 0
-
-for c in s:
-    if c in "aeiou":
-        count += 1
-
-print "Number of vowels: " + str(count)
